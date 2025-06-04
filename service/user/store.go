b7c21e8dad258974180101145823b15e12323fe4
Oliver Kwun-Morfitt <oliverkwunmorfitt@gmail.com>
2025-06-04 15:56:38 -0400

    added user scanning

--- service/user/store.go
+++ service/user/store.go
@@ -1,1 +1,45 @@
 package user
+
+import (
+	"database/sql"
+
+	"github.com/olliekm/gorestapi/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+
+	}
+}
+
+func scanRowIntoUser(rows *sql.Row) (*types.User, error) {
+	user := new(types.User)
+
+	err := rows.Scan(
+		&user.ID,
+		&user.FirstName,
+		&user.LastName,
+		&user.Email,
+		&user.Password,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
